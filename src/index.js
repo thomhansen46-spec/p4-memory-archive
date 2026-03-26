64269e210d847a4326a9cd231621d585e1983112
thomhansen46-spec <thomhansen46@gmail.com>
2026-03-26 15:32:30 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,9 +3,6 @@ const cors = require('cors');
 const fetch = require('node-fetch');
 const app = express();
 
-
-app.use(cors());
-app.use(express.json());
 app.use(cors());
 app.use(express.json());
 app.get('/', (req, res) => {
